69777df2e65431b5b5d58ebdf8c46b1d53a9eafd
name <quuip01@gmail.com>
2021-03-14 17:45:33 +0700

    1

---
+++ Project1/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button1 = new System.Windows.Forms.Button();
+            this.LeftButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Discolor = new System.Windows.Forms.CheckBox();
+            this.Noise = new System.Windows.Forms.CheckBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Button1
+            // 
+            this.Button1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.Button1.Location = new System.Drawing.Point(477, 430);
+            this.Button1.Margin = new System.Windows.Forms.Padding(4);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(100, 28);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "Next";
+            this.Button1.UseVisualStyleBackColor = false;
+            this.Button1.Click += new System.EventHandler(this.ButtonImageChange);
+            // 
+            // LeftButton
+            // 
+            this.LeftButton.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.LeftButton.Location = new System.Drawing.Point(275, 430);
+            this.LeftButton.Margin = new System.Windows.Forms.Padding(4);
+            this.LeftButton.Name = "LeftButton";
+            this.LeftButton.Size = new System.Drawing.Size(100, 28);
+            this.LeftButton.TabIndex = 1;
+            this.LeftButton.Text = "Back";
+            this.LeftButton.UseVisualStyleBackColor = false;
+            this.LeftButton.Click += new System.EventHandler(this.ButtonImageChange);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(275, 105);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(303, 293);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 43);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "TakePicture";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(16, 105);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(216, 356);
+            this.listBox1.TabIndex = 4;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // Discolor
+            // 
+            this.Discolor.AutoSize = true;
+            this.Discolor.Location = new System.Drawing.Point(605, 105);
+            this.Discolor.Name = "Discolor";
+            this.Discolor.Size = new System.Drawing.Size(81, 21);
+            this.Discolor.TabIndex = 5;
+            this.Discolor.Text = "Discolor";
+            this.Discolor.UseVisualStyleBackColor = true;
+            this.Discolor.CheckedChanged += new System.EventHandler(this.Discolor_CheckedChanged);
+            // 
+            // Noise
+            // 
+            this.Noise.AutoSize = true;
+            this.Noise.Location = new System.Drawing.Point(605, 146);
+            this.Noise.Name = "Noise";
+            this.Noise.Size = new System.Drawing.Size(93, 21);
+            this.Noise.TabIndex = 6;
+            this.Noise.Text = "Add noise";
+            this.Noise.UseVisualStyleBackColor = true;
+            this.Noise.CheckedChanged += new System.EventHandler(this.Noise_CheckedChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(704, 198);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = -100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(176, 56);
+            this.trackBar1.TabIndex = 7;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(602, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Brightness";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(602, 256);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 17);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Transparency";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(704, 260);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(176, 56);
+            this.trackBar2.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 527);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.Noise);
+            this.Controls.Add(this.Discolor);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.LeftButton);
+            this.Controls.Add(this.Button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button LeftButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox Discolor;
+        private System.Windows.Forms.CheckBox Noise;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar2;
+    }
+}
+
---
+++ Project1/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    public partial class Form1 : Form
+    {
+        private List<FileInfo> _imageList;
+        private int _imageIndex;
+        float b;
+        bool gray;
+        Image original;
+        bool noise;
+        public Form1()
+        {
+            InitializeComponent();
+            _imageList = new List<FileInfo>();
+            gray = false;
+            noise = false;
+        }
+
+
+    private void Button2_Click(object sender, EventArgs e)
+        {
+            var fb = new FolderBrowserDialog();
+            if (fb.ShowDialog() == DialogResult.OK)
+            {
+                var folder = new DirectoryInfo(fb.SelectedPath);
+                _imageList = folder.GetFiles()
+                        .Where(c => c.Extension == ".jpg"
+                        || c.Extension == ".png")
+                        .ToList();
+
+                //_imageList.Clear();
+                //_imageList.AddRange(images);
+
+                listBox1.Items.Clear();
+                foreach (var item in _imageList)
+                {
+                    listBox1.Items.Add(item.Name);
+                }
+                DisplayImage();
+            }
+        }
+        private void DisplayImage()
+        {
+            var image = _imageList.ElementAtOrDefault(_imageIndex);
+            if (image != null)
+            {
+                pictureBox1.Image?.Dispose();
+                pictureBox1.Image = Image.FromFile(image.FullName);
+                original = Image.FromFile(image.FullName);
+            }
+        }
+
+        private void ButtonImageChange(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            switch (button.Name)
+            {
+                case "button1":
+                    _imageIndex++;
+                    break;
+                case "LeftButton":
+                    _imageIndex--;
+                    break;
+            }
+        }
+        private void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var listbox = sender as ListBox;
+            _imageIndex = listbox.SelectedIndex;
+            DisplayImage();
+        }
+
+
+        private void Discolor_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender; 
+            if (checkBox.Checked == true)
+            {
+                gray = true;
+                ChangePictures();
+            }
+            else
+            {
+                gray = false;
+                ChangePictures();
+            }
+        }
+
+        private void Noise_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox checkBox = (CheckBox)sender; 
+            if (checkBox.Checked == true)
+            {
+                noise = true;
+                ChangePictures();
+            }
+            else
+            {
+                noise = false;
+                ChangePictures();
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            b = (float)trackBar1.Value / 100;
+            ChangePictures();
+
+        }
+
+        private void ChangePictures()
+        {
+
+
+            float[][] matrix = {
+                    new float[] { 1, 0, 0, 0, 0 } ,
+                    new float[] { 0, 1, 0, 0, 0 } ,
+                    new float[] { 0, 0, 1, 0, 0 } ,
+                    new float[] { 0, 0, 0, 1, 0 } ,
+                    new float[] { b, b, b, 0, 1 }
+                 };
+            float[][] matrix2 = {
+                    new float[] { 0.299f, 0.299f, 0.299f, 0, 0 },
+                    new float[] { 0.587f, 0.587f, 0.587f, 0, 0 },
+                    new float[] { 0.114f, 0.114f, 0.114f, 0, 0 },
+                    new float[] { 0, 0, 0, 1, 0 },
+                    new float[] { b, b, b, 0, 1 },
+              };
+            var image = new Bitmap(original.Width, original.Height);
+
+            ImageAttributes attr = new ImageAttributes();
+            if (gray)
+            {
+                attr.SetColorMatrix(new ColorMatrix(matrix2));
+            }
+            else
+                attr.SetColorMatrix(new ColorMatrix(matrix));
+            if (pictureBox1.Image != null)
+            {
+                using (Graphics g = Graphics.FromImage(image))
+                {
+                    Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+                    g.DrawImage(original, rect, 0, 0, original.Width, original.Height, GraphicsUnit.Pixel, attr);
+                }
+                if (noise)
+                {
+                    pictureBox1.Image = CreateNoise(image);
+                    return;
+                }
+                pictureBox1.Image = image;
+            }
+        }
+
+        
+        private Bitmap CreateNoise(Bitmap imageNoise)
+        {
+            Random rnd = new Random();
+
+
+            for (int i = 0; i < imageNoise.Width; i++)
+            {
+                for (int j = 0; j < imageNoise.Height; j++)
+                {
+                    Color color = imageNoise.GetPixel(i, j);
+                    byte r = (byte)(rnd.Next(0, 2) == 1 ? color.R : 255);
+                    byte b = (byte)(rnd.Next(0, 2) == 1 ? color.B : 255);
+                    byte g = (byte)(rnd.Next(0, 2) == 1 ? color.G : 255);
+
+                    imageNoise.SetPixel(i, j, Color.FromArgb(255, r, g, b));
+                }
+            }
+            return imageNoise;
+        }
+    }
+}
